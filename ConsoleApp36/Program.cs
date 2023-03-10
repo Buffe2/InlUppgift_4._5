11412ed9d0703b974732b4589a4467fd287db173
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-03-10 08:50:56 +0100

    Add project files.

---
+++ ConsoleApp36/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace InlUppgift_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Skriv in ett heltal, hur stort som helst");
+            string tal = Console.ReadLine();
+
+            for(int i = 0; i < tal.Length; i++)
+            {
+                string tal2 = tal[i].ToString();
+                int siffra = Convert.ToInt32(tal2);
+
+                if (siffra == 9)
+                    Console.WriteLine(0);
+                else
+                    Console.WriteLine(siffra+1);
+                    
+            }
+        
+        }
+
+    }
+
+}
+
